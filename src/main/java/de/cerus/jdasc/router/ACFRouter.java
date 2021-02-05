6b0aab0703c6f4fd8616333051ff4e76593db57b
Cerus <m.dorn.2003@web.de>
2021-02-05 03:18:31 +0100

    Fix bunch of message related stuff

--- src/main/java/de/cerus/jdasc/router/ACFRouter.java
+++ src/main/java/de/cerus/jdasc/router/ACFRouter.java
@@ -5,15 +5,26 @@ import de.cerus.jdasc.command.ApplicationCommand;
 import de.cerus.jdasc.interaction.Interaction;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
 import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.entities.Category;
+import net.dv8tion.jda.api.entities.ChannelType;
+import net.dv8tion.jda.api.entities.Emote;
 import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.IMentionable;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.entities.MessageReaction;
+import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.internal.entities.DataMessage;
+import org.apache.commons.collections4.Bag;
+import org.apache.commons.collections4.bag.HashBag;
 import org.jetbrains.annotations.NotNull;
 
 public class ACFRouter extends CommandRouter {
@@ -66,6 +77,95 @@ public class ACFRouter extends CommandRouter {
             @Override
             public String getId() {
                 return String.valueOf(this.getIdLong());
+            }
+
+            @NotNull
+            @Override
+            public ChannelType getChannelType() {
+                return this.getChannel().getType();
+            }
+
+            @NotNull
+            @Override
+            public Bag<Emote> getEmotesBag() {
+                return new HashBag<>();
+            }
+
+            @NotNull
+            @Override
+            public Bag<Role> getMentionedRolesBag() {
+                return new HashBag<>();
+            }
+
+            @NotNull
+            @Override
+            public Bag<TextChannel> getMentionedChannelsBag() {
+                return new HashBag<>();
+            }
+
+            @NotNull
+            @Override
+            public Bag<User> getMentionedUsersBag() {
+                return new HashBag<>();
+            }
+
+            @Override
+            public Category getCategory() {
+                return this.getGuild().getCategories().get(0);
+            }
+
+            @NotNull
+            @Override
+            public List<IMentionable> getMentions(@NotNull final MentionType... types) {
+                return new ArrayList<>();
+            }
+
+            @NotNull
+            @Override
+            public List<Emote> getEmotes() {
+                return new ArrayList<>();
+            }
+
+            @NotNull
+            @Override
+            public List<Member> getMentionedMembers() {
+                return new ArrayList<>();
+            }
+
+            @NotNull
+            @Override
+            public List<Member> getMentionedMembers(@NotNull final Guild guild) {
+                return new ArrayList<>();
+            }
+
+            @NotNull
+            @Override
+            public List<Role> getMentionedRoles() {
+                return new ArrayList<>();
+            }
+
+            @NotNull
+            @Override
+            public List<TextChannel> getMentionedChannels() {
+                return new ArrayList<>();
+            }
+
+            @NotNull
+            @Override
+            public List<User> getMentionedUsers() {
+                return new ArrayList<>();
+            }
+
+            @NotNull
+            @Override
+            public List<MessageEmbed> getEmbeds() {
+                return new ArrayList<>();
+            }
+
+            @NotNull
+            @Override
+            public List<MessageReaction> getReactions() {
+                return new ArrayList<>();
             }
         };
 
