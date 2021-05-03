08ee59fb248c751a8007a361fb0ec40775d8a5ca
Pringles <80323633+PringlePot@users.noreply.github.com>
2021-05-03 20:59:08 +0200

    Add support for DM commands

--- src/main/java/de/cerus/jdasc/listener/InteractionListener.java
+++ src/main/java/de/cerus/jdasc/listener/InteractionListener.java
@@ -1,5 +1,6 @@
 package de.cerus.jdasc.listener;
 
+import co.aikar.commands.annotation.Private;
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
@@ -14,6 +15,7 @@ import java.util.List;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.PrivateChannel;
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.GenericEvent;
 import net.dv8tion.jda.api.events.RawGatewayEvent;
@@ -45,12 +47,21 @@ public class InteractionListener extends ListenerAdapter {
             }
 
             final JDA jda = event.getJDA();
-            final Guild guild = jda.getGuildById(object.get("guild_id").getAsString());
-            final Member member = guild.retrieveMemberById(object.get("member").getAsJsonObject().get("user").getAsJsonObject().get("id").getAsString()).complete();
-            final TextChannel textChannel = guild.getTextChannelById(object.get("channel_id").getAsString());
+            if(object.has("guild_id")){
+                final Guild guild = jda.getGuildById(object.get("guild_id").getAsString());
+                final Member member = guild.retrieveMemberById(object.get("member").getAsJsonObject().get("user").getAsJsonObject().get("id").getAsString()).complete();
+                final TextChannel textChannel = guild.getTextChannelById(object.get("channel_id").getAsString());
+                final Interaction interaction = new Interaction(token, id, type, commandId, commandName, guild, textChannel, member, options);
+                jda.getEventManager().handle(new CommandInteractionEvent(jda, interaction));
+            }
+            //Direct messages
+            else{
+                final PrivateChannel textChannel = jda.getPrivateChannelById(object.get("channel_id").getAsString());
+                final Interaction interaction = new Interaction(token, id, type, commandId, commandName, null, (TextChannel) textChannel, null, options);
+                jda.getEventManager().handle(new CommandInteractionEvent(jda, interaction));
 
-            final Interaction interaction = new Interaction(token, id, type, commandId, commandName, guild, textChannel, member, options);
-            jda.getEventManager().handle(new CommandInteractionEvent(jda, interaction));
+            }
+
         }
     }
 
